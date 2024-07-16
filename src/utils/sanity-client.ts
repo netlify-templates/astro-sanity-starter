8b834e6cb1a50ede00bbde39077456b3bad67289
Suzanne Aitchison <suzanne.aitchison@netlify.com>
2024-07-16 10:18:09 +0100

    Update sanity-client.ts

--- src/utils/sanity-client.ts
+++ src/utils/sanity-client.ts
@@ -1,7 +1,7 @@
 import { loadEnv } from 'vite';
 import { createClient, type ClientConfig } from '@sanity/client';
 
-const { SANITY_PROJECT_ID, SANITY_DATASET, SANITY_ACCESS_TOKEN, STACKBIT_PREVIEW, SANITY_PREVIEW_DRAFTS } = loadEnv(
+const { SANITY_PROJECT_ID, SANITY_DATASET, SANITY_TOKEN, STACKBIT_PREVIEW, SANITY_PREVIEW_DRAFTS } = loadEnv(
     process.env.NODE_ENV || '',
     process.cwd(),
     ''
@@ -15,7 +15,7 @@ export const sanityConfig: ClientConfig = {
     dataset: SANITY_DATASET || 'production',
     useCdn: false,
     apiVersion: '2024-01-31',
-    token: SANITY_ACCESS_TOKEN,
+    token: SANITY_TOKEN,
     perspective: isDev || isDeployPreview || previewDrafts ? 'previewDrafts' : 'published'
 };
 
