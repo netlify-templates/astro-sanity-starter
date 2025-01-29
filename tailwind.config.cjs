9eb115e45ae1e0b4d075c42e0e9a72942b37680c
TomasBankauskas <tbdizainas@gmail.com>
2025-01-29 15:38:29 +0200

    updated tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -16,46 +16,5 @@ export default {
                 sans: ['Mulish', ...defaultTheme.fontFamily.sans]
             }
         }
-    },
-    plugins: [require('daisyui')],
-    daisyui: {
-        themes: [
-            {
-                dark: {
-                    ...require('daisyui/src/theming/themes')['dark'],
-                    primary: '#ffae9c',
-                    'primary-content': '#171227',
-                    secondary: '#fff2d7',
-                    'secondary-content': '#171227',
-                    accent: '#e3f1ff',
-                    'accent-content': '#171227',
-                    neutral: '#2e293c',
-                    'neutral-content': '#ffffff',
-                    'base-100': '#171227',
-                    'base-200': '#2e293c',
-                    'base-content': '#ffffff',
-                    '--rounded-btn': '.375rem',
-                    '--rounded-badge': '9999px'
-                }
-            },
-            {
-                light: {
-                    ...require('daisyui/src/theming/themes')['light'],
-                    primary: '#ffae9c',
-                    'primary-content': '#171227',
-                    secondary: '#fff2d7',
-                    'secondary-content': '#171227',
-                    accent: '#e3f1ff',
-                    'accent-content': '#171227',
-                    neutral: '#171227',
-                    'neutral-content': '#ffffff',
-                    'base-100': '#ffffff',
-                    'base-200': '#d7d8e4',
-                    'base-content': '#171227',
-                    '--rounded-btn': '.375rem',
-                    '--rounded-badge': '9999px'
-                }
-            }
-        ]
     }
 };
