56fcce291d8ce040dfd09651ebd68a1b6d78639d
TomasBankauskas <tbdizainas@gmail.com>
2025-03-29 10:10:54 +0200

    updated npm dependencies and config files

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -1,17 +1,17 @@
-import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
-import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemaTypes'
-import {markdownSchema} from 'sanity-plugin-markdown'
+import { defineConfig } from 'sanity'
+import { deskTool } from 'sanity/desk'
+import { visionTool } from '@sanity/vision'
+import { schemaTypes } from './schemaTypes'
+import { markdownSchema } from 'sanity-plugin-markdown'
 
 export default defineConfig({
   name: 'default',
   title: 'Astro Sanity Starter',
 
-  projectId: process.env.SANITY_STUDIO_PROJECT_ID ?? process.env.SANITY_PROJECT_ID,
-  dataset: process.env.SANITY_STUDIO_DATASET ?? process.env.SANITY_DATASET,
+  projectId: (process.env.SANITY_STUDIO_PROJECT_ID ?? process.env.SANITY_PROJECT_ID) as string,
+  dataset: (process.env.SANITY_STUDIO_DATASET ?? process.env.SANITY_DATASET) as string,
 
-  plugins: [structureTool(), visionTool(), markdownSchema()],
+  plugins: [deskTool(), visionTool(), markdownSchema()],
 
   schema: {
     types: schemaTypes,
